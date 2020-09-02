5d56f22e77779489118ff21a2e0a7ef3d734c0e3
zahrakhan <zahra.khan1@icloud.com>
2020-09-02 17:39:13 -0400

    organized folders, added fonts, updated favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
+import About from './screens/About/About'
+import Home from './screens/Home/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+    </Switch>
     </div>
   );
 }
