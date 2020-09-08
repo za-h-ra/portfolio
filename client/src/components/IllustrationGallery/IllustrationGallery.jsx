c37da939c12194be41e55109add5b8c598ae61ba
zahrakhan <zahra.khan1@icloud.com>
2020-09-08 17:14:43 -0400

    create and style illustration screen

---
+++ client/src/components/IllustrationGallery/IllustrationGallery.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const GalleryCard = styled.div``
+
+export default function IllustrationGallery() {
+	return (
+		<GalleryCard>
+			<a href={require('../../illustrations/20.png')}>
+				<img src={require('../../illustrations/20.png')} />
+			</a>
+
+			<a href={require('../../illustrations/13.png')}>
+				<img src={require('../../illustrations/13.png')} />
+			</a>
+
+			<a href={require('../../illustrations/22.png')}>
+				<img src={require('../../illustrations/22.png')} />
+			</a>
+
+			<a href={require('../../illustrations/17.png')}>
+				<img src={require('../../illustrations/17.png')} />
+			</a>
+
+			<a href={require('../../illustrations/5.png')}>
+				<img src={require('../../illustrations/5.png')} />
+			</a>
+
+			<a href={require('../../illustrations/3.png')}>
+				<img src={require('../../illustrations/3.png')} />
+			</a>
+
+			<a href={require('../../illustrations/7.png')}>
+				<img src={require('../../illustrations/7.png')} />
+			</a>
+
+			<a href={require('../../illustrations/23.png')}>
+				<img src={require('../../illustrations/23.png')} />
+			</a>
+
+			<a href={require('../../illustrations/24.png')}>
+				<img src={require('../../illustrations/24.png')} />
+			</a>
+
+			<a href={require('../../illustrations/19.png')}>
+				<img src={require('../../illustrations/19.png')} />
+			</a>
+
+			<a href={require('../../illustrations/18.png')}>
+				<img src={require('../../illustrations/18.png')} />
+			</a>
+
+			<a href={require('../../illustrations/16.png')}>
+				<img src={require('../../illustrations/16.png')} />
+			</a>
+
+			<a href={require('../../illustrations/15.png')}>
+				<img src={require('../../illustrations/15.png')} />
+			</a>
+
+			<a href={require('../../illustrations/14.png')}>
+				<img src={require('../../illustrations/14.png')} />
+			</a>
+
+			<a href={require('../../illustrations/12.png')}>
+				<img src={require('../../illustrations/12.png')} />
+			</a>
+
+			<a href={require('../../illustrations/11.png')}>
+				<img src={require('../../illustrations/11.png')} />
+			</a>
+
+			<a href={require('../../illustrations/10.png')}>
+				<img src={require('../../illustrations/10.png')} />
+			</a>
+
+			<a href={require('../../illustrations/9.png')}>
+				<img src={require('../../illustrations/9.png')} />
+			</a>
+
+			<a href={require('../../illustrations/8.png')}>
+				<img src={require('../../illustrations/8.png')} />
+			</a>
+
+			<a href={require('../../illustrations/21.png')}>
+				<img src={require('../../illustrations/21.png')} />
+			</a>
+
+			<a href={require('../../illustrations/1.png')}>
+				<img src={require('../../illustrations/1.png')} />
+			</a>
+
+			<a href={require('../../illustrations/26.PNG')}>
+				<img src={require('../../illustrations/26.PNG')} />
+			</a>
+
+			<a href={require('../../illustrations/2.png')}>
+				<img src={require('../../illustrations/2.png')} />
+			</a>
+
+			<a href={require('../../illustrations/4.png')}>
+				<img src={require('../../illustrations/4.png')} />
+			</a>
+
+			<a href={require('../../illustrations/6.png')}>
+				<img src={require('../../illustrations/6.png')} />
+			</a>
+
+			<a href={require('../../illustrations/25.png')}>
+				<img src={require('../../illustrations/25.png')} />
+			</a>
+		</GalleryCard>
+	)
+}
--- client/src/screens/Illustrations/Illustrations.jsx
+++ client/src/screens/Illustrations/Illustrations.jsx
@@ -2,19 +2,42 @@ import React from 'react'
 import styled from 'styled-components'
 import { colors } from '../../styles/global'
 import Nav from '../../components/shared/Nav/Nav'
+import IllustrationGallery from '../../components/IllustrationGallery/IllustrationGallery'
 
-const StyledDiv = styled.div`
+const StyledNav = styled.div`
 	a {
-    color: ${colors.black};
-    font-weight: 600;
+		color: ${colors.black};
+		font-weight: 600;
+	}
+`
+
+const GalleryContainer = styled.div``
+
+const GalleryInner = styled.div``
+
+const Border = styled.div``
+
+const Gallery = styled.div`
+	img {
+		height: 300px;
 	}
 `
 
 export default function Illustrations() {
-  return (
-    <StyledDiv>
-      <Nav />
-      <h1>TBD</h1>
-    </StyledDiv>
-  )
+	return (
+		<>
+			<StyledNav>
+				<Nav />
+			</StyledNav>
+			<GalleryContainer>
+				<GalleryInner>
+					<h1>Illustrations</h1>
+					<Border></Border>
+					<Gallery>
+            <IllustrationGallery />
+					</Gallery>
+				</GalleryInner>
+			</GalleryContainer>
+		</>
+	)
 }
